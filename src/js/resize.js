55e323079ed38618f444979a68d37e171f083977
Aaron Geller <a-geller@northwestern.edu>
2020-10-08 10:40:15 -0500

    initial commit

---
+++ src/js/resize.js
@@ -0,0 +1,35 @@
+//attach listener on window resize and run it once to start
+window.addEventListener("resize", resizer);
+resizer();
+
+function resizer(){
+
+	var lim = 0.1*window.innerHeight; //pixel limit to allow
+	var nLim = 50; //limit the number of iterations for text resizing
+
+	//resize the paragraph font size so the para fills the screen height
+	var para = document.getElementById('paraColumn');
+	var fs = parseFloat(window.getComputedStyle(para, null).getPropertyValue('font-size'));
+	var rect = para.getBoundingClientRect();
+	var height = window.innerHeight - rect.top;
+	var diff = height - rect.height;
+	var nTrial = 0;
+	console.log(fs)
+	console.log(nTrial, height, rect.height, diff, lim, fs)
+	var fac = 0.5;
+
+	while ((Math.abs(diff) > lim || diff < 0) & nTrial < nLim){
+		var mult = fac*(1. - nTrial/nLim);
+		if (diff > 0){
+			mult += 1.;
+		}
+		para.style.fontSize = fs*mult + 'px';
+		fs = parseFloat(window.getComputedStyle(para, null).getPropertyValue('font-size'));
+		rect = para.getBoundingClientRect();
+		diff = height - rect.height;
+
+		nTrial += 1
+		console.log(nTrial, rect.height, diff, fs, mult)
+	}
+	document.getElementById('form').height = rect.height;
+}
